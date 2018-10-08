acec9d473cd54bb0e04a9359dbb5019334b7a985
wesley.sun <wesley_agile@aliyun.com>
2018-10-08 23:10:10 +0800

    SparkContext

--- core/src/main/scala/com/pptb/spark/SparkContext.scala
+++ core/src/main/scala/com/pptb/spark/SparkContext.scala
@@ -1,5 +1,20 @@
 package com.pptb.spark
 
-object SparkContext {
+class SparkContext(
+                    master: String,
+                    frameworkName: String,
+                    val sparkHome: String = null,
+                    val jars: Seq[String] = Nil
+                  ) extends Logging {
+  //Ensure logging is initialized before we spawn any threads
+  initLogging()
+
+  // Set Spark master host and port system properties
+  if (System.getProperty("spark.master.host") == null) {
+    System.setProperty("spark.master.host", Utils.localIpAddress)
+  }
+  if (System.getProperty("spark.master.port") == null) {
+    System.setProperty("spark.master.port", "7077")
+  }
 
 }
